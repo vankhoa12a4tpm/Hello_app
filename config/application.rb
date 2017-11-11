f631757b7f07ffceae48ef2c1c19ab7aa40a6cff
[DN-OE9]Nguyen nhu van khoa <vankhoa12a4@gmail.com>
2017-11-11 18:03:38 +0700

    Chapter 5

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
 
-    config.i18n.default_locale = :vi
+    config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
