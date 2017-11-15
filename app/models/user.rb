0096bc9f560bc68bf55fdc79efd1ca220da547d0
[DN-OE9]Nguyen nhu van khoa <vankhoa12a4@gmail.com>
2017-11-15 13:18:09 +0700

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,5 @@
 class User < ApplicationRecord
   before_save{self.email = email.downcase}
-<<<<<<< HEAD
-  validates :name, presence: true, length: {maximum: 50}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: 255},
-                    format: {with: VALID_EMAIL_REGEX},
-                    uniqueness: {case_sensitive: false}
-  has_secure_password
-  validates :password, presence: true, length: {minimum: 6}
-=======
   validates :name, presence: true, length: {maximum: Settings.user.maximun_name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: Settings.user.maximun_email},
@@ -17,5 +8,4 @@ class User < ApplicationRecord
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.user.minimum_pass}
->>>>>>> 98a89db... Make a basic User model (including secure passwords)
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,12 +1,4 @@
 require "test_helper"
-<<<<<<< HEAD
-
-=======
-<<<<<<< 829db4848b7bed0faadfd73d9d03e9e5f5cb0936
-
-=======
->>>>>>> Make a basic User model (including secure passwords)
->>>>>>> 98a89db... Make a basic User model (including secure passwords)
 class UserTest < ActiveSupport::TestCase
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
@@ -16,7 +8,6 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
-
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 5
     assert_not @user.valid?
