b84086c205c51925d14880bafc6df85337ff1e46
[DN-OE9]Nguyen nhu van khoa <vankhoa12a4@gmail.com>
2017-11-19 10:10:26 +0700

    chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
   before_save{self.email = email.downcase}
-  validates :name, presence: true, length: {maximum: Settings.user.maximun_name}
+  validates :name, presence: true, length: {maximum: Settings.user.maximum_name}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: {maximum: Settings.user.maximun_email},
+  validates :email, presence: true, length: {maximum: Settings.user.maximum_email},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
-  attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.user.minimum_pass}
 end
